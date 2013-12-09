18e209ce028e0c8a5c7f8bea8a96bc35825fecec
Nick Bastin <nick.bastin@gmail.com>
2013-12-08 23:32:22 -0800

    Add GCF package

---
+++ geni/aggregate/cmd.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2013  Barnstormer Softworks, Ltd.
+
+from __future__ import absolute_import
+
+from geni.gcf import oscript
+
+def listresources (am, slice = None):
+  text, res = oscript.call(["-a", am, "listresources"])
+  if res.values()[0]["code"]["geni_code"] == 0:
+    rspec = res.values()[0]["value"]
+    return rspec
