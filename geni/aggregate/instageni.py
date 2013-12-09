a3b85ebfc287fea12365445f356376e85c511c2a
Nick Bastin <nick.bastin@gmail.com>
2013-12-09 04:01:04 -0800

    Wrapper class for instageni compute AMs

--- geni/aggregate/instageni.py
+++ geni/aggregate/instageni.py
@@ -7,10 +7,14 @@ import sys
 
 from . import AM
 
-GPO = AM("ig-gpo", "https://boss.instageni.gpolab.bbn.com:12369/protogeni/xmlrpc/am/2.0", "amapiv2", "pg")
-Kentucky = AM("ig-kentucky", "https://boss.lan.sdn.uky.edu:12369/protogeni/xmlrpc/am/2.0", "amapiv2", "pg")
-Utah = AM("ig-utah", "https://boss.utah.geniracks.net:12369/protogeni/xmlrpc/am/2.0", "amapiv2", "pg")
-UtahDDC = AM("ig-utahddc", "https://boss.utahddc.geniracks.net:12369/protogeni/xmlrpc/am/2.0", "amapiv2", "pg")
+class PGAM(AM):
+  def __init__ (self, name, url):
+    super(IGCompute, self).__init__(name, url, "amapiv2", "pg")
+
+GPO = PGAM("ig-gpo", "https://boss.instageni.gpolab.bbn.com:12369/protogeni/xmlrpc/am/2.0")
+Kentucky = PGAM("ig-kentucky", "https://boss.lan.sdn.uky.edu:12369/protogeni/xmlrpc/am/2.0")
+Utah = PGAM("ig-utah", "https://boss.utah.geniracks.net:12369/protogeni/xmlrpc/am/2.0")
+UtahDDC = PGAM("ig-utahddc", "https://boss.utahddc.geniracks.net:12369/protogeni/xmlrpc/am/2.0")
 
 def aggregates ():
   module = sys.modules[__name__]
