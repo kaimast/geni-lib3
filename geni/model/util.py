cca17c0bc01af3978c103f8bb660e3437f7ee19b
Nick Bastin <nick.bastin@gmail.com>
2014-03-07 11:39:13 -0800

    Typos..

--- geni/model/util.py
+++ geni/model/util.py
@@ -3,10 +3,11 @@
 class XPathXRange(object):
   def __init__ (self, xp, klass):
     self._data = xp
+    self._klass = klass
   def __iter__ (self):
     for obj in self._data:
-      yield klass._fromdom(vlan)
+      yield self._klass._fromdom(vlan)
   def __len__ (self):
     return len(self._data)
   def __getitem__ (self, idx):
-    return klass._fromdom(self._data[idx])
+    return self._klass._fromdom(self._data[idx])
