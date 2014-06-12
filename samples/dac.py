c010db8b09b09f9135cb30f4ffb7910c06e5e6d7
Nick Bastin <nick.bastin@gmail.com>
2014-06-11 21:35:11 -0700

    dac tweak (this script no longer appears to create non-trivial lans, unfortunately)

--- samples/dac.py
+++ samples/dac.py
@@ -5,32 +5,29 @@ import geni.aggregate.instageni as IG
 import geni.aggregate.apis
 
 import nbastin
-
 context = nbastin.buildContext()
-context.debug = True
 
-try:
-  IG.GPO.deletesliver(context, "xen-test2")
-except geni.aggregate.apis.AMError, e:
-  pass
+IG.UtahDDC.deletesliver(context, "xen-test3")
 
 r = PG.Request()
 
 vm1 = PG.XenVM("xen1")
 intf1 = vm1.addInterface("if0")
+intf1.component_id = "eth1"
 r.addResource(vm1)
 
 vm2 = PG.XenVM("xen2")
-intf2 = vm2.addInterface("if0")
+intf2 = vm2.addInterface("if1")
+intf2.component_id = "eth2"
 r.addResource(vm2)
 
 lnk = PG.Link()
 lnk.addInterface(intf1)
 lnk.addInterface(intf2)
-lnk.bandwidth = 1000000
-lnk.disableMACLearning()
 
 r.addResource(lnk)
 
-manifest = IG.GPO.createsliver(context, "xen-test2", r)
+r.write("dac.xml")
+
+manifest = IG.UtahDDC.createsliver(context, "xen-test3", r)
 print manifest.text
