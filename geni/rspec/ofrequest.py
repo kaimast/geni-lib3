2acb3d5b86116a8af749fa09070caf8f3ed8c41e
Nick Bastin <nick.bastin@gmail.com>
2014-07-17 22:34:25 -0700

    Add missing OFR file

---
+++ geni/rspec/ofrequest.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2014  Barnstormer Softworks, Ltd.
+
+from . import RSpec
+
+class Request(RSpec):
+  def __init__ (self):
+    super(Request, self).__init__("request")
+    self.resources = []
+
+  def addGroup (self, name):
+    group = Group(name)
+    self.resources.append(group)
+    return group
+
+  def buildMatch (self):
+    match = Match()
+    self.resources.append(match)
+    return match
+
+class Datapath(object):
+  def __init__ (self, ad_dp = None):
+    self._ad_dp = ad_dp
