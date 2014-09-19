422ffac8b4a6c2e4f5bd301c3f7b6e2287cd842f
Nick Bastin <nick.bastin@gmail.com>
2014-09-19 06:40:02 -0700

    Docs for IG extensions

--- geni/rspec/igext.py
+++ geni/rspec/igext.py
@@ -7,6 +7,14 @@ from lxml import etree as ET
 from .pg import Namespaces as PGNS
 
 class OFController(object):
+  """OpenFlow controller specification to be used on a PG VLAN.
+  
+Add to link objects using the Link.addChild() method.
+
+NOTE: This will have no effect if a trivial link is created by the aggregate.
+You need to make sure that a VLAN will be provisioned (typically by making sure
+that at least two interfaces on the link are on different physical hosts)."""
+
   def __init__ (self, host, port=6633):
     self.host = host
     self.port = port
