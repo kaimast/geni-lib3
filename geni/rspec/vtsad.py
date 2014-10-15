03a08f0d23ffeb43a0a4e559ad34011993f95341
Nick Bastin <nick.bastin@gmail.com>
2014-10-15 11:08:05 -0700

    Circuit plane advertisement parsing

--- geni/rspec/vtsad.py
+++ geni/rspec/vtsad.py
@@ -2,7 +2,33 @@
 
 from __future__ import absolute_import
 
+import geni.namespaces as GNS
+from geni.model.util import XPathXRange
+
 from lxml import etree as ET
+
+VTSNS = GNS.Namespace("vts", "http://geni.bssoftworks.com/rspec/ext/vts/ad/1")
+_XPNS = {'g' : GNS.REQUEST.name, 'v' : VTSNS.name}
+
+class CircuitPlane(object):
+  def __init__ (self):
+    self.label = None
+    self.mtu = None
+    self.type = None
+    self.endpoint = None
+    self.tunnel_types = []
+
+  @classmethod
+  def _fromdom (cls, elem):
+    cp = CircuitPlane()
+    cp.label = elem.get("label")
+    cp.mtu = int(elem.get("mtu"))
+    supported = elem.xpath('v:supported-tunnels/v:tunnel-type', namespaces = _XPNS)
+    for tuntyp in supported:
+      cp.tunnel_types.append(tuntyp.get("name"))
+    cp.endpoint = elem.xpath('v:endpoint', namespaces = _XPNS)[0].get("value")
+    return cp
+
 
 class Advertisement(object):
   def __init__ (self, path = None, xml = None):
@@ -11,6 +37,10 @@ class Advertisement(object):
     elif xml:
       self._root = ET.fromstring(xml)
 
+  @property
+  def circuit_planes (self):
+    return XPathXRange(self._root.xpath("v:circuit-planes/v:circuit-plane", namespaces = _XPNS), CircuitPlane)
+    
   @property
   def text (self):
     return ET.tostring(self._root, pretty_print=True)
