6a65fa990c95a61ffbec98cd84c59cb91cf2c024
Nick Bastin <nick.bastin@gmail.com>
2014-10-18 12:56:39 -0700

    Add scripts directory

--- support/envsetup.bat
+++ support/envsetup.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 set PYTHONPATH=C:\Python27\Lib;C:\Python27\libs;C:\Program Files\omniTools-2.7;C:\Program Files\omniTools-2.7\Library.zip;
-set PATH=%PATH%;C:\Python27
+set PATH=%PATH%;C:\Python27;C:\Python27\Scripts
