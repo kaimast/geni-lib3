5de3d062a8563f025f3d14c2d6232976d39be9ac
Robert Ricci <ricci@cs.utah.edu>
2014-12-28 22:03:05 -0700

    Add example script for the portal context

---
+++ samples/portal-context.py
@@ -0,0 +1,32 @@
+import geni.portal as portal
+import geni.rspec.pg as RSpec
+
+hw_types = ["m400", "dl360" ]
+
+pc = portal.Context()
+
+pc.defineParameter("N", "Number of nodes",
+                   portal.ParameterType.INTEGER, 5)
+pc.defineParameter("hwtype", "Hardware type of all nodes",
+                   portal.ParameterType.NODETYPE, hw_types[0], hw_types)
+pc.defineParameter("lan",  "Put all nodes in a LAN",
+                   portal.ParameterType.BOOLEAN, False)
+
+params = pc.bindParameters()
+
+rspec = RSpec.Request()
+
+if (params['lan']):
+    lan = RSpec.LAN()
+    rspec.addResource(lan)
+
+for i in range(1, params['N']+1):
+  node = RSpec.RawPC("node%d" % i)
+  node.hardware_type = params['hwtype']
+  rspec.addResource(node)
+
+  if params['lan']:
+    iface = node.addInterface("eth0")
+    lan.addInterface(iface)
+
+pc.printRequestRSpec(rspec)
