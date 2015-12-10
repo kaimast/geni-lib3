1d913cb6834c6af070c65321b153dbde072c401a
Nick Bastin <nick.bastin@gmail.com>
2015-12-10 06:10:11 -0800

    Update minigcf docs

--- geni/minigcf/config.py
+++ geni/minigcf/config.py
@@ -5,7 +5,13 @@
 #  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class HTTP(object):
+  """Global configuration options for MiniGCF HTTP(S) calls."""
+
   TIMEOUT = 30
+  """Initial response timeout.  Note that this is not the time limit on the entire
+  download, just the initial server response."""
+
   ALLOW_REDIRECTS = False
+  """Allow MiniGCF to follow HTTP redirects (301)."""
 
 
