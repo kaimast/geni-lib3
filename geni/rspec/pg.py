6426b73f9404345234220c5b2f551f3cf2da168d
Nick Bastin <nick.bastin@gmail.com>
2015-12-13 07:28:39 -0800

    Updated copyright, added license stub

--- geni/rspec/pg.py
+++ geni/rspec/pg.py
@@ -1,4 +1,8 @@
-# Copyright (c) 2013-2014  Barnstormer Softworks, Ltd.
+# Copyright (c) 2013-2015  Barnstormer Softworks, Ltd.
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from __future__ import absolute_import
 
