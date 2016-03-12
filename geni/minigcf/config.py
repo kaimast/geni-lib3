31708b7078adb65736538b3a4d2e420636fed67c
Nick Bastin <nick.bastin@gmail.com>
2016-03-12 15:55:20 -0800

    Transparently support logging requests via xmlrpclib.dumps hook

--- geni/minigcf/config.py
+++ geni/minigcf/config.py
@@ -4,6 +4,8 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import xmlrpclib
+
 class HTTP(object):
   """Global configuration options for MiniGCF HTTP(S) calls."""
 
@@ -21,3 +23,12 @@ class HTTP(object):
   LOG_RAW_REQUESTS = False
   """If set to a valid `(log_handle, log_level)` tuple, will write all raw requests
   (before any parsing) to AM API and CH API calls to that log at the given level."""
+
+def dumps_with_log (*args, **kwargs):
+  req_data = xmlrpclib._dumps(*args, **kwargs)
+  if isinstance(HTTP.LOG_RAW_REQUESTS, tuple):
+    HTTP.LOG_RAW_REQUESTS[0].log(HTTP.LOG_RAW_REQUESTS[1], req_data)
+  return req_data
+
+setattr(xmlrpclib, "_dumps", getattr(xmlrpclib, "dumps"))
+setattr(xmlrpclib, "dumps", dumps_with_log)
