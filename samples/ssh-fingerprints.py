5e77d2877ef444c24acd3a5c6729374a3ce3c6b3
Nick Bastin <nick.bastin@gmail.com>
2016-05-24 01:23:43 -0700

    Add license statements

--- samples/ssh-fingerprints.py
+++ samples/ssh-fingerprints.py
@@ -2,6 +2,11 @@
 
 # Copyright (c) 2016  Barnstormer Softworks, Ltd.
 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
 import argparse
 import hashlib
 import base64
