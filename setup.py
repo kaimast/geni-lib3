af4886304fe86ba8b65ecb8d7b5bd2338461bd31
Nick Bastin <nick.bastin@gmail.com>
2016-12-28 22:24:18 -0500

    Make trusty and xenial match crypto versions

--- setup.py
+++ setup.py
@@ -8,11 +8,20 @@ from setuptools import setup, find_packages
 
 import os
 import os.path
+import platform
+
+requires = ["wrapt"]
 
 # If you are on linux, and don't have ca-certs, we can do an awful thing and it will still work
-if os.name == "posix" and os.uname()[0] == "Linux" and not os.path.exists("/etc/ssl/certs/ca-certificates.crt"):
-  import ssl
-  ssl._create_default_https_context = ssl._create_unverified_context
+if os.name == "posix" and os.uname()[0] == "Linux":
+  if not os.path.exists("/etc/ssl/certs/ca-certificates.crt"):
+    import ssl
+    ssl._create_default_https_context = ssl._create_unverified_context
+
+  # On Xenial you installed cryptography from the dist
+  (name, ver, dist_id) = platform.linux_distribution()
+  if dist_id == "trusty":
+    requires.append("cryptography==1.2.3")
 
 setup(name = 'geni-lib',
       version = '0.9.3',
@@ -21,10 +30,7 @@ setup(name = 'geni-lib',
       packages = find_packages(),
       scripts = ['tools/buildcontext/context-from-bundle',
                  'tools/shell/genish'],
-      install_requires = [
-        "cryptography==1.6",
-        "wrapt",
-        ],
+      install_requires = requires,
       classifiers = [
         "Programming Language :: Python :: 2.7",
         "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
