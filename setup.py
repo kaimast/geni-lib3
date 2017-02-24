e503c4530699ffee6587202ddd699acaab89856e
Nick Bastin <nick.bastin@gmail.com>
2017-02-24 06:11:02 -0500

    Start using build numbers

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ if os.name == "posix" and os.uname()[0] == "Linux":
     requires.append("cryptography==1.2.3")
 
 setup(name = 'geni-lib',
-      version = '0.9.3',
+      version = '0.9.4.1',
       author = 'Nick Bastin',
       author_email = 'nick@bssoftworks.com',
       packages = find_packages(),
