23f062c2104b2389fc9e564d82bfef0093ce7778
Nick Bastin <nick.bastin@gmail.com>
2017-05-30 12:04:56 -0400

    Bad namespace

--- geni/aggregate/vts.py
+++ geni/aggregate/vts.py
@@ -78,7 +78,7 @@ class VTS(AM):
     self.IPv4Router = v4RouterPOAs(self)
 
   def allocate (self, context, sname, rspec):
-    manifest = self._apiv3.allocate(context, self.am.urlv3, sname, rspec)
+    manifest = self._apiv3.allocate(context, self.urlv3, sname, rspec)
     return self.amtype.parseManifest(manifest)
 
   def provision (self, context, sname):
@@ -87,7 +87,7 @@ class VTS(AM):
       data = {"urn" : user.urn, "keys" : [open(x, "rb").read() for x in user._keys]}
       udata.append(data)
 
-    return self._apiv3.provision(context, self.am.urlv3, sname, options = {"geni_users" : udata})
+    return self._apiv3.provision(context, self.urlv3, sname, options = {"geni_users" : udata})
 
   def changeController (self, context, sname, url, datapaths, ofver=None):
     options={"controller-url" : url, "datapaths" : datapaths}
