1c80abc2560158413914e787efcbbb278fee52a3
Nick Bastin <nick.bastin@gmail.com>
2018-02-11 02:05:50 -0500

    Missing subpackages

--- setup.py
+++ setup.py
@@ -22,14 +22,17 @@ if os.name == "posix" and os.uname()[0] == "Linux":
     import ssl
     ssl._create_default_https_context = ssl._create_unverified_context
 
+pkgs = find_packages()
+pkgs.append('')
+
 setup(name = 'geni-lib',
-      version = '0.9.7.5',
+      version = '0.9.7.6',
       author = 'Nick Bastin',
       author_email = 'nick@bssoftworks.com',
       description = 'Library and tools for working with research testbed resources that support ' \
                     'the GENI AM API, including the NSF GENI Testbed (www.geni.net) and Cloudlab (cloudlab.us).',
       long_description = open("README.rst", "r").read(),
-      packages = ['', 'geni', 'ccloud'],
+      packages = pkgs,
       package_dir = {'' : 'tools/ipython', 'geni' : 'geni', 'ccloud' : 'ccloud'},
       pymodules = ['genish'],
       scripts = ['tools/buildcontext/context-from-bundle',
