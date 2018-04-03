638b205bd458175c7667daab8489faf12bd06778
Nick Bastin <nick.bastin@gmail.com>
2018-04-03 01:39:28 -0400

    Fix dates

--- geni/support/ipython.py
+++ geni/support/ipython.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2015-2017  Barnstormer Softworks, Ltd.
+# Copyright (c) 2015-2018  Barnstormer Softworks, Ltd.
 
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
