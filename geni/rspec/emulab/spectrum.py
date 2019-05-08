ea7e64af4c5eafa7a6bbdd739e3b3d756519fc50
Leigh Stoller <Leigh Stoller@localhost>
2019-05-08 17:26:51 -0600

    Powder specific extensions for requesting radio spectrum

---
+++ geni/rspec/emulab/spectrum.py
@@ -0,0 +1,29 @@
+# Copyright (c) 2016-2019 The University of Utah
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from __future__ import absolute_import
+
+from ..pg import Request, Namespaces, Link, Request, Node, Interface
+import geni.namespaces as GNS
+from lxml import etree as ET
+
+class requestSpectrum(object):
+    def __init__(self, freq_low, freq_high, power):
+        self._freq_low  = freq_low
+        self._freq_high = freq_high
+        self._power     = power
+    
+    def _write(self, root):
+        el = ET.SubElement(root, "{%s}spectrum" % (Namespaces.EMULAB.name))
+        el.attrib["frequency_low"]  = str(self._freq_low)
+        el.attrib["frequency_high"] = str(self._freq_high)
+        el.attrib["power"]          = str(self._power)
+        return root
+
+Node.EXTENSIONS.append(("requestSpectrum", requestSpectrum))
+Interface.EXTENSIONS.append(("requestSpectrum", requestSpectrum))
+Request.EXTENSIONS.append(("requestSpectrum", requestSpectrum))
+
